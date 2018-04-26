90289435dfb9b73326fd9f0fdabdcfa13f21d40b
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-04-25 21:21:22 -0300

    Set up save and load ann models

--- Project/ann.py
+++ Project/ann.py
@@ -16,11 +16,11 @@ y = dataset.iloc[:, 12].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# # Feature Scaling
-# from sklearn.preprocessing import StandardScaler
-# sc = StandardScaler()
-# X_train = sc.fit_transform(X_train)
-# X_test = sc.transform(X_test)
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 # integer encode
 label_encoder = LabelEncoder()
