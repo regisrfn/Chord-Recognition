1f645c829ac760d1d242720bd23aa53a011f82ed
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-06-06 15:59:15 -0300

    Principal component analysis (PCA)

--- Project/PCA.py
+++ Project/PCA.py
@@ -26,7 +26,7 @@ from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
-explained_variance = pca.explained_variance_ratio_
+explained_variance = pca.components_
 
 # Fitting K-NN to the Training set
 from sklearn.neighbors import KNeighborsClassifier
